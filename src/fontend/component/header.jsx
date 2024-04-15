c87052c52401a80b1a465ef64e765d425de90d6e
Bienfait1992 <georgesakake@gmail.com>
2024-04-15 16:51:41 +0100

    Nav_bar creat

--- src/fontend/component/header.jsx
+++ src/fontend/component/header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Nav_bar from "./nav_bar";
+
 
 
 function Header() {
@@ -6,12 +8,13 @@ function Header() {
         <>
             <div className="header">
                 <div><img src="src/fontend/images/images.png" className="logo"></img></div>
-                <div><input type="text" placeholder="Qu'est ce vous recherceher?"  className="recherche"/></div>
+                <div><input type="text" placeholder="Qu'est ce vous recherceher?" className="recherche" /></div>
                 <div><img src="src/fontend/icones/user_11129843.png" className="user"></img></div>
                 <button>Sign in</button>
                 <button>Sign Up</button>
-
+                <div><img src="src/fontend/icones/conversation-svgrepo-com.svg" alt="" className="panier" /></div>
             </div>
+<Nav_bar/>
         </>
     )
 }
---
+++ src/fontend/component/nav_bar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Nav_bar(){
+    return(
+        <>
+         <div className="nav_bar">Navigation</div>
+        </>
+       
+    )
+}
+export default Nav_bar;
